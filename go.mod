c664032c4c0f44528263f5afea76453842ac041c
lewisb1989 <lewisbarber1989@gmail.com>
2025-05-17 18:15:16 +0100

    add background music

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module tetris
 go 1.23.0
 
 require (
+	github.com/hajimehoshi/go-mp3 v0.3.4
+	github.com/hajimehoshi/oto v1.0.1
 	github.com/nsf/termbox-go v1.1.1
 	github.com/stretchr/testify v1.10.0
 )
@@ -12,5 +14,9 @@ require (
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
+	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
+	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
+	golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,45 @@
 package main
 
 import (
+	"github.com/hajimehoshi/go-mp3"
+	"github.com/hajimehoshi/oto"
 	term "github.com/nsf/termbox-go"
+	"io"
+	"os"
 	"tetris/game"
 	"time"
 )
+
+func playAudio() {
+	f, err := os.Open("tetris.mp3")
+	if err != nil {
+		panic(err)
+	}
+	d, err := mp3.NewDecoder(f)
+	if err != nil {
+		panic(err)
+	}
+	c, err := oto.NewContext(d.SampleRate(), 2, 2, 8192)
+	if err != nil {
+		panic(err)
+	}
+	p := c.NewPlayer()
+	if _, err := io.Copy(p, d); err != nil {
+		panic(err)
+	}
+	err = f.Close()
+	if err != nil {
+		panic(err)
+	}
+	err = c.Close()
+	if err != nil {
+		panic(err)
+	}
+	err = p.Close()
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	err := term.Init()
@@ -13,6 +48,7 @@ func main() {
 	}
 	defer term.Close()
 	tetris := game.NewTetris(10, 20, time.Millisecond*333)
+	go playAudio()
 	for {
 		ev := term.PollEvent()
 		if ev.Key == term.KeyEsc {
