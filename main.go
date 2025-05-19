aa18ec757007639428f033601107c696e8669260
lewisb1989 <lewisbarber1989@gmail.com>
2025-05-19 09:16:31 +0100

    new screenshot

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 		panic(err)
 	}
 	defer term.Close()
-	tetris := game.NewTetris(10, 40, time.Millisecond*666)
+	tetris := game.NewTetris(10, 20, time.Millisecond*333)
 	for {
 		ev := term.PollEvent()
 		if ev.Key == term.KeyEsc {
